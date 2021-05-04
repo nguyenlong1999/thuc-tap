6cb0625bc10de1ef40f4ae464e47e90e86e897ea
ImissYss <76909298+ImissYss@users.noreply.github.com>
2021-05-04 15:35:39 +0700

    depot2

--- custom/thuc_tap/models/depot.py
+++ custom/thuc_tap/models/depot.py
@@ -3,10 +3,16 @@ from odoo import models, fields
 
 class Depot(models.Model):
     _name = 'mg.depot'
-    _inherit ='mg.address'
     _description = 'Information Depots'
 
     name = fields.Char(string='Name', required=True)
+    street = fields.Char(string='Street', required=True)
+    zip = fields.Char(string='ZIP')
+    city = fields.Char(string='City')
+    country_id = fields.Many2one('res.country', string='Country')
+    province_id = fields.Many2one('res.country.state', string='Province', domain="[('country_id','=',country_id)]")
+    district = fields.Char(string='District')
+    ward = fields.Char(string='Ward')
     latitude = fields.Float(string='latitude')
     longitude = fields.Float(string='longitude')
     status = fields.Selection({
