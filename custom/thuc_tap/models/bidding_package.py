1c85253c41d1d58ce3901677fcbbf7ca8bd15300
QUAN <sackalac00@gmail.com>
2021-05-05 11:09:18 +0700

    QUAN NEW
    update

--- custom/thuc_tap/models/bidding_package.py
+++ custom/thuc_tap/models/bidding_package.py
@@ -1,3 +1,5 @@
+import datetime
+
 from odoo import fields, models, exceptions, api
 import re
 
@@ -9,7 +11,7 @@ class BiddingPackage(models.Model):
     _description = "Package Record"
     _rec_name = "name"
 
-    name = fields.Char(string="Name package", required=True)
+    name = fields.Char(string="Name package")
     code = fields.Char(string='Code')
     status = fields.Selection([
         ("0", "chưa nhận"),
@@ -22,24 +24,30 @@ class BiddingPackage(models.Model):
     return_date = fields.Datetime(string="Return date", required=True)
     from_address = fields.Char(string="From Address", required=True)
     to_address = fields.Char(string="To Address", required=True)
-    is_publish = fields.Boolean(char="Is public", required=False, default=False)
-    is_auto = fields.Char(string="Auto", required=True)
-    publish_time = fields.Integer(string="Public time", readonly=True)
-    publish_time_plan = fields.Char(string="Public time plan", required=True)
-    duration_time = fields.Integer(string="Duration time", required=True)
+    is_publish = fields.Boolean(char="Is public", default=False)
+    is_auto = fields.Boolean(string="Auto", default=False)
+    publish_time = fields.Datetime(datetime="Public time", readonly=True)
+    publish_time_plan = fields.Datetime(datetime="Public time plan")
+    duration_time = fields.Integer(integer="Duration time")
     is_real = fields.Boolean(string="Is real package", default=True)
-    cargo_id = fields.Many2many("mg.cargo", 'id', String="Cargo")
+    cargo_id = fields.Many2many("mg.cargo", 'id', string="Cargo")
 
     @api.onchange('return_date')
     def onchange_return_date(self):
         for bdp in self:
             if bdp.return_date is not False:
-                if bdp.return_date <= bdp.receive_date or bdp.receive_date < datetime.datetime.now():
+                if bdp.return_date <= bdp.receive_date or bdp.return_date <= datetime.datetime.now():
                     raise exceptions.ValidationError("Impossible")
 
-    def check_expiry(self):
-        today = fields.Datetime.now()
+    def _check_expiry(self):
         package_orders = self.env['mg.bidding.package'].search([])
         for order in package_orders:
-            if order.publish_time_plan == today:
+            today = fields.Datetime.now()
+            if order.publish_time_plan <= today:
                 order.is_publish = True
+                if order.publish_time is False:
+                    order.publish_time = fields.Datetime.now()
+            if order.publish_time is not False:
+                duration = datetime.timedelta(minutes=order.duration_time) + order.publish_time
+                if duration <= fields.Datetime.now():
+                    order.is_publish = False
