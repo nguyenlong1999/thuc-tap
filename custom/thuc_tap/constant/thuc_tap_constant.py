dafcff789855083126ec1077c94167a6d41897c3
sonnt2990 <nguyentruongson2k0929@gmail.com>
2021-05-06 21:26:48 +0700

    add tag, constant

---
+++ custom/thuc_tap/constant/thuc_tap_constant.py
@@ -0,0 +1,3 @@
+class Constant():
+    API_MAP_ROUTE_URL = 'https://maps.googleapis.com/maps/api/directions/json?mode=driving&transit_routing_preference=less_driving'
+    API_MAP_ROUTE_KEY = 'AIzaSyCmzEKuZOtAuDR5-iHmJvLScbSolUJEhBk'
---
+++ custom/thuc_tap/constant/thuc_tap_status_tag.py
@@ -0,0 +1,8 @@
+class StatusTag():
+    # tag cho status
+    STATUS_CANCEL = '-1'  # Hủy
+    STATUS_UNCONFIMRED = '0'  # Chưa nhận
+    STATUS_APPROVED = '1'  # Đã duyệt
+    STATUS_WAIT = '2'  # Chờ xác nhận
+    STATUS_RECEIVED = '3'  # Đã nhận
+    STATUS_PAID = '4'  # Đã trả
