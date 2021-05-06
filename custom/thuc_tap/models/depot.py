2a9747e7e7f61426b2c0abf86cf4cb60fe1cdf31
ImissYss <76909298+ImissYss@users.noreply.github.com>
2021-05-06 08:22:25 +0700

    depot map

--- custom/thuc_tap/models/depot.py
+++ custom/thuc_tap/models/depot.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Depot(models.Model):
@@ -10,7 +10,7 @@ class Depot(models.Model):
     zip = fields.Char(string='ZIP')
     city = fields.Char(string='City')
     country_id = fields.Many2one('res.country', string='Country')
-    province_id = fields.Many2one('res.country.state', string='Province', domain="[('country_id','=',country_id)]")
+    state_id = fields.Many2one('res.country.state', string='Province', domain="[('country_id','=',country_id)]")
     district = fields.Char(string='District')
     ward = fields.Char(string='Ward')
     latitude = fields.Float(string='latitude')
@@ -19,3 +19,28 @@ class Depot(models.Model):
         ('1', 'active'),
         ('-1', 'deactive')
     })
+
+    @api.model
+    def _geo_localize(self, street='', zip='', city='', state='', country=''):
+        geo_obj = self.env['base.geocoder']
+        search = geo_obj.geo_query_address(street=street, zip=zip, city=city, state=state, country=country)
+        result = geo_obj.geo_find(search, force_country=country)
+        if result is None:
+            search = geo_obj.geo_query_address(city=city, state=state, country=country)
+            result = geo_obj.geo_find(search, force_country=country)
+        return result
+
+    def geo_localize(self):
+        for depot in self.with_context(lang='en_US'):
+            result = self._geo_localize(depot.street,
+                                        depot.zip,
+                                        depot.city,
+                                        depot.state_id.name,
+                                        depot.country_id.name)
+
+            if result:
+                depot.write({
+                    'latitude': result[0],
+                    'longitude': result[1],
+                })
+        return True
